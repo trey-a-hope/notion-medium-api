c638c5c3f5d7a698c5c7236456c2ca4f1877fa16
Trey Hope <99027548+trey-a-hope@users.noreply.github.com>
2024-11-20 11:15:27 -0500

    wip
    wip

--- src/index.ts
+++ src/index.ts
@@ -86,7 +86,9 @@ app.listen(port, () => {
 });
 
 async function getPage() {
-  const { title, icon, cover, html } = await NotionPageToHtml.convert("https://www.notion.so/asnunes/Simple-Page-Text-4d64bbc0634d4758befa85c5a3a6c22f");
+  // const { title, icon, cover, html } = await NotionPageToHtml.convert("https://www.notion.so/asnunes/Simple-Page-Text-4d64bbc0634d4758befa85c5a3a6c22f");
+  const res = await NotionPageToHtml.convert("https://www.notion.so/asnunes/Simple-Page-Text-4d64bbc0634d4758befa85c5a3a6c22f");
+
   // console.log(title, icon, cover, html);
   return 'OK BOY!';
 }
